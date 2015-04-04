9476859cf111a8e9980cdce430d3a95a9c7c9331
SageHack <sage@cyberguerrilla.org>
2015-04-04 11:39:09 -0400

    Finished CF_Detect class properly

--- cfresolv.py
+++ cfresolv.py
@@ -1,28 +1,53 @@
-from ipaddress import IPv4Network
-from ipaddress import IPv6Network
+from ipaddress import IPv4Address, IPv4Network, IPv6Address, IPv6Network
 
 
 class CF_Detect:
 
-    IPV4_NETWORK = [
+    IPV4_NETWORKS = [
         IPv4Network(network)
         for network
         in open('ips-v4').read().splitlines()
     ]
 
-    IPV6_NETWORK = [
+    IPV6_NETWORKS = [
         IPv6Network(network)
         for network
         in open('ips-v6').read().splitlines()
     ]
 
-    def __init__(self):
-        return None
-
     def in_range(self, ip):
-        print(self.IPV4_NETWORK)
-        print(self.IPV6_NETWORK)
+        address = self.instantiate_address(ip)
+        if not address:
+            return False
+
+        if address.version == 4:
+            networks = self.IPV4_NETWORKS
+        else:
+            networks = self.IPV6_NETWORKS
+
+        return self.in_network(address, networks)
+
+    def in_network(self, host, networks):
+        for network in networks:
+            if host in network:
+                return True
+
+        return False
+
+    def instantiate_address(self, ip):
+        try:
+            return IPv4Address(ip)
+        except:
+            try:
+                return IPv6Address(ip)
+            except:
+                return None
 
 
 cf = CF_Detect()
-cf.in_range('127.0.0.1')
+print(cf.in_range('adfasdf'))
+print(cf.in_range(''))
+print(cf.in_range('172.71.255.250'))
+print(cf.in_range('250.71.255.250'))
+print(cf.in_range('2400:cb00::5e:b4da'))
+print(cf.in_range('9400:cb00::5e:b4da'))
