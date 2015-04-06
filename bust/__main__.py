3438710554ff901ec816d7cc4c177fc14d11d1bc
SageHack <sage@cyberguerrilla.org>
2015-04-06 10:15:32 -0400

    Moved main functions into its own class. Fixed problem with subdomain list

--- bust/__main__.py
+++ bust/__main__.py
@@ -1,6 +1,5 @@
 import argparse
-from detect import Detector
-from target import Target
+from buster import CloudBuster
 
 parser = argparse.ArgumentParser(
     description='CloudFlare pentest tool'
@@ -34,60 +33,15 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-
-def check_ip(ip):
-    detector = Detector()
-    cf_owned = detector.in_range(ip)
-    print(cf_owned)
-
-
-def target(domain, args):
-    target = Target(domain)
-    target.option('name', 'Target')
-    target.scan()
-    target.infos()
-    if target.on_cloudflare():
-        scan_subdomains(domain)
-        if args.panels or args.panels == []:
-            scan_panels(domain, args)
-
-
-def scan_subdomains(domain):
-    subs = [sub for sub in open('lists/subdomains').read().splitlines()]
-    for sub in subs:
-        subdomain = sub+'.'+domain
-        target = Target(subdomain)
-        target.option('name', 'Subdomain')
-        target.scan()
-        target.infos()
-
-
-def scan_panels(domain, args):
-    # http://www.mysql-apache-php.com/ports.htm
-    panels = [
-        {'name': 'cpanel', 'port': 2082, 'ssl': False},
-        {'name': 'cpanel-ssl', 'port': 2083, 'ssl': True},
-        {'name': 'whm', 'port': 2086, 'ssl': False},
-        {'name': 'whm-ssl', 'port': 2087, 'ssl': True},
-        {'name': 'plesk', 'port': 8087, 'ssl': False},
-        {'name': 'plesk-ssl', 'port': 8443, 'ssl': True},
-    ]
-
-    for panel in panels:
-        if args.panels == [] or panel['name'] in args.panels:
-            target = Target(
-                domain+':'+str(panel['port'])
-            )
-            target.option('name', 'Pannel ('+panel['name']+')')
-            target.option('timeout', 1)
-            target.option('ssl', panel['ssl'])
-            target.scan()
-            target.infos()
-
+buster = CloudBuster(args.target)
 
 if args.check_ip:
-    check_ip(args.check_ip)
+    buster.check_ip(args.check_ip)
 elif args.target:
-    target(args.target, args)
+    buster.target()
+    if buster.target_on_cloudflare():
+        buster.scan_subdomains()
+        if args.panels or args.panels == []:
+            buster.scan_panels(args.panels)
 else:
     parser.print_help()
