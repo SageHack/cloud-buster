5c9302da287aae05d7b69e3a3d01d4b7ba2bf79c
SageHack <sage@cyberguerrilla.org>
2015-04-17 23:16:38 -0400

    Removed error file. Not sure how I'll manage errors in the future. We will see.

--- bust/errors.py
+++
@@ -1,6 +0,0 @@
-class DNSPythonError:
-    def output():
-        print('== MX ==')
-        print(': Cannot scan MX records')
-        print(': dnspython not installed')
-        print(': check README.md to install')
