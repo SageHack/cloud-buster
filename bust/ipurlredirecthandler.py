3b305a4d6eccf588521807b7dca974a7120ccec8
SageHack <sagehack@riseup.net>
2018-03-23 06:38:40 -0400

    Added useful redirection output

--- bust/ipurlredirecthandler.py
+++ bust/ipurlredirecthandler.py
@@ -5,6 +5,7 @@ class IpUrlRedirectHandler(request.HTTPRedirectHandler):
     def redirect_request(self, req, fp, code, msg, hdrs, newurl):
 
         if 'Host' not in req.headers:
+            print('** redirect: '+newurl)
             return super(IpUrlRedirectHandler, self).redirect_request(req, fp, code, msg, hdrs, newurl)
         
         original_url = urlparse(req.get_full_url())
@@ -16,4 +17,5 @@ class IpUrlRedirectHandler(request.HTTPRedirectHandler):
         )
         req.headers['Host'] = new_url.hostname
 
+        print('** redirect: '+new_url.hostname+'@'+custom_url)
         return super(IpUrlRedirectHandler, self).redirect_request(req, fp, code, msg, hdrs, custom_url)
