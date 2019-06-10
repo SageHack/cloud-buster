66b7da2d7dedd479a89da34fda14182aef83584a
SageHack <sagehack@riseup.net>
2019-06-10 18:14:59 +0200

    pep8ing

--- test_all.py
+++ test_all.py
@@ -1,9 +1,15 @@
 import subprocess
 
-def bust(args, expected_returncode=0, expected_stdout=None, expected_stderr=None):
 
-    base_args=['python3', 'bust']
-    full_args=base_args + args
+def bust(
+        args,
+        expected_returncode=0,
+        expected_stdout=None,
+        expected_stderr=None
+        ):
+
+    base_args = ['python3', 'bust']
+    full_args = base_args + args
 
     exec = subprocess.run(
         full_args,
@@ -22,7 +28,7 @@ def bust(args, expected_returncode=0, expected_stdout=None, expected_stderr=None
             print(exec)
             return False
     else:
-        if expected_stdout != None:
+        if expected_stdout is not None:
             return False
 
     if expected_stderr is not None:
@@ -31,22 +37,38 @@ def bust(args, expected_returncode=0, expected_stdout=None, expected_stderr=None
             print(exec)
             return False
     else:
-        if expected_stderr != None:
+        if expected_stderr is not None:
             return False
 
     return True
 
+
 def test_fail_without_args():
-    assert bust([], 2, None, 'required: DOMAIN') == True
+    assert bust(
+        [], 2, None, 'required: DOMAIN'
+    ) is True
+
 
 def test_exec_succeed_showing_help():
-    assert bust(['-h'], 0, 'usage:', None) == True
+    assert bust(
+        ['-h'], 0, 'usage:', None
+    ) is True
+
 
 def test_error_on_target_not_behind_cloudflares():
-    assert bust(['hacklair.cyberguerrilla.org'], 0, 'not behind Cloudflare', None) == True
+    assert bust(
+        ['hacklair.cyberguerrilla.org'], 0, 'not behind Cloudflare', None
+    ) is True
+
 
 def test_unresolvable_domain():
-    assert bust(['buster-test-9000.tk'], 0, 'cannot resolve host', None) == True
+    assert bust(
+        ['buster-test-9000.tk'], 0, 'cannot resolve host', None
+    ) is True
+
 
 def test_match_crimeflare():
-    assert bust(['thecatholicdirectory.com', '--scan', 'crimeflare'], 0, '[match] thecatholicdirectory.com', None) == True
+    assert bust(
+        ['thecatholicdirectory.com', '--scan', 'crimeflare'],
+        0, '[match] thecatholicdirectory.com', None
+    ) is True
