ed375b35890fbeb92df36ab6f0b1f5aa498e68d0
SageHack <sagehack@riseup.net>
2019-06-10 16:26:57 +0200

    Improved tests

--- tests/test_main_exec.py
+++ tests/test_main_exec.py
@@ -1,13 +1,44 @@
-from os import system
+import subprocess
 
 def fail_without_args():
-    return system('python3 bust')
+    exec = subprocess.run(
+        args=['python3', 'bust'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+
+    if exec.returncode != 2:
+        print('Error: bad return code')
+        print(exec)
+        return False
+
+    if 'required: DOMAIN' not in exec.stderr.decode('utf-8'):
+        print('Error: process does not fail as expected')
+        print(exec)
+        return False
+
+    return True
 
 def test_fail_without_args():
-    assert fail_without_args() == 512
+    assert fail_without_args() == True
 
 def exec_succeed_showing_help():
-    return system('python3 bust -h')
+    exec = subprocess.run(
+        args=['python3', 'bust', '-h'],
+        stdout=subprocess.PIPE
+    )
+
+    if exec.returncode != 0:
+        print('Error: bad return code')
+        print(exec)
+        return False
+
+    if 'usage:' not in exec.stdout.decode('utf-8'):
+        print('Error: unable to find expected text')
+        print(exec)
+        return False
+
+    return True
 
 def test_exec_succeed_showing_help():
-    assert exec_succeed_showing_help() == 0
+    assert exec_succeed_showing_help() == True
--- tests/test_target_unprotected.py
+++ tests/test_target_unprotected.py
@@ -1,13 +1,22 @@
 import subprocess
 
 def error_on_target_not_behind_cloudflare():
-    return subprocess.run(
+    exec = subprocess.run(
         args=['python3', 'bust', 'hacklair.cyberguerrilla.org'],
         stdout=subprocess.PIPE
     )
 
+    if exec.returncode != 0:
+        print('Error: bad return code')
+        print(exec)
+        return False
+
+    if 'not behind Cloudflare' not in exec.stdout.decode('utf-8'):
+        print('Error: unable to find the expected string')
+        print(exec)
+        return False
+
+    return True
+
 def test_error_on_target_not_behind_cloudflares():
-    process = error_on_target_not_behind_cloudflare()
-    print(process)
-    stdout = process.stdout.decode("utf-8")
-    assert stdout.find('not behind Cloudflare')
+    assert error_on_target_not_behind_cloudflare() == True
